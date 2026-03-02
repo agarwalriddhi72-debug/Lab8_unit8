875a38e799216188d32fdf3de35fb0b18cb7aae2
Riddhi <agarwal.riddhi72@gmail.com>
2026-03-02 10:52:00 -0500

    Calculating area and perimeter of a rectangle

--- rectangle.py
+++ rectangle.py
@@ -5,3 +5,13 @@ Purpose of program: To calculate area and perimeter of a rectangle
 Starter code: None
 Date: March 2, 2026
 """
+
+import math
+
+#Calculate area of a rectangle
+def area(width, height):
+    return width * height
+
+#Calculate perimeter of a rectangle
+def perimeter(width, height):
+    return 2*width + 2*height

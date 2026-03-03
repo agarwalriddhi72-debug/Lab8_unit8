4445b3a715cd24317850c7c1375a913ea44ff00c
Riddhi <agarwal.riddhi72@gmail.com>
2026-03-03 14:12:54 -0500

    Added documentation in rectangle module

--- rectangle.py
+++ rectangle.py
@@ -1,17 +1,37 @@
 """
-Program name: Geometry calculator
+Program name: Geometry calculator - Rectangle Module
 Name: Riddhi Agarwal
-Purpose of program: To calculate area and perimeter of a rectangle 
+Date: March 3, 2026
 Starter code: None
-Date: March 2, 2026
+
+Purpose of program: 
+To calculate area and perimeter of a rectangle.
+
+Functions:
+- area(width, height): Returns the area of the rectangle.
+- perimeter(width, height): Returns the perimeter of the rectangle.
 """
 
 import math
 
-#Calculate area of a rectangle
 def area(width, height):
+    """
+    Calculates the area of a rectangle
+    Parameters:
+        width (float): The width of the rectangle
+        height (float): The height of the rectangle
+    Returns:
+        float: The area of the rectangle
+    """
     return width * height
 
-#Calculate perimeter of a rectangle
 def perimeter(width, height):
+    """
+    Calculates the perimeter of the rectangle
+    Parameters:
+        width (float): The width of the rectangle
+        height (float): The height of the rectangle
+    Returns:
+        float: The perimeter of the rectangle
+    """
     return 2*width + 2*height

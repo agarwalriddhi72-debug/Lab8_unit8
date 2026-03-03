038f3a2f23958f9511bf90d37f00d8c8a8dc9880
Riddhi <agarwal.riddhi72@gmail.com>
2026-03-03 14:10:10 -0500

    Added documentation in circle module

--- circle.py
+++ circle.py
@@ -1,17 +1,34 @@
 """
-Program name: Geometry calculator
+Program name: Geometry calculator - Circle Module
 Name: Riddhi Agarwal
-Purpose of program: To calculate area and perimeter of a circle 
+Date: March 3, 2026
 Starter code: None
-Date: March 2, 2026
+
+Purpose of program: 
+To calculate area and perimeter of a circle.
+
+Functions:
+- area(radius): Returns the area of a circle given the radius
+- circumference(radius): Returns the circumference of a circle given the radius
 """
 
 import math
 
-#Calculates the area of a circle
 def area(radius):
+    """
+    Calculates the area of a circle.
+    Parameters:
+        radius (float): The readius of the circle
+    Returns: float: The are of the circle
+    """
     return math.pi * (radius ** 2)
 
-#Calculate the perimeter of a circle
 def circumference(radius): 
+    """
+    Calculates the circumference of a circle.
+    Parameters:
+        radius (float): The radius of the circle.
+    Returns:
+        float: The circumference of the circle
+    """
     return 2 * math.pi * radius
